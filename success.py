219be874f650f1b04b42465b4d615179162c5a36
pr3Cash <blogger.thomasjerry@gmail.com>
2024-04-11 00:50:27 +0530

    single folder success excluding the hsbc and uala

--- success.py
+++ success.py
@@ -64,19 +64,48 @@ def extract_data_from_image(image_path):
 
         # hold this becauze it has the 'Santander' word in the middle of the text
         elif "bna" in line.lower():
-            print(line.lower())
+            # print(line.lower())
             bank_name = "BNA"
             break
 
         # hold this becauze it has the 'Santander' word in the middle of the text
         elif "supervielle" in line.lower():
-            print(line.lower())
+            # print(line.lower())
             bank_name = "SUPERVIELLE"
             break
 
         elif "bancociudad" in line.lower():
-            print(line.lower())
+            # print(line.lower())
             bank_name = "BancoCiudad"
+            break
+
+        elif "banco santa fe" in line.lower():
+            # print(line.lower())
+            bank_name = "Banco Santa Fe"
+            break
+
+        elif "bbva" in line.lower():
+            # print(line.lower())
+            bank_name = "BBVA"
+            break
+
+        elif "naranja x" in line.lower():
+            # print(line.lower())
+            bank_name = "Naranja X"
+            break
+
+        elif "banco credicoop coop. ltdo" in line.lower():
+            # print(line.lower())
+            bank_name = "Banco Credicoop Coop. Ltdo"
+            break
+
+        elif "personal pay" in line.lower():
+            # print(line.lower())
+            bank_name = "Personal Pay"
+            break
+
+        elif "bancor" in line.lower():
+            bank_name = "Bancor"
             break
 
     if bank_name == "Bancopatagonia":
@@ -267,6 +296,206 @@ def extract_data_from_image(image_path):
         cuit = cuit_found[1] if cuit_found else None
         proof_number = proof_number_found[1] if proof_number_found else None
 
+    elif bank_name == "Banco Santa Fe":
+
+        line_24 = lines[24] 
+
+        bank_pattern = 'Banco Santa Fe'
+        date_pattern = r'\b\d{1,2}/\d{1,2}/\d{4}\b'
+        amount_pattern = r'\$\s*\d[\d,\.]*'
+        proof_number_pattern = r'\b\d{8}\b'
+        payer_name_pattern = line_24
+        cuit_pattern = r'\b\d{11}\b'
+
+        bank_found = re.findall(bank_pattern, extracted_text, re.IGNORECASE)
+        dates_found = re.findall(date_pattern, extracted_text)
+        amounts_found = re.findall(amount_pattern, extracted_text)
+        payer_name_found = re.findall(payer_name_pattern, extracted_text)
+        cuit_found = re.findall(cuit_pattern, extracted_text)
+        proof_number_found = re.findall(proof_number_pattern, extracted_text)
+
+        bank = bank_found[0] if bank_found else None
+        date = dates_found[0] if dates_found else None
+        amount = amounts_found[0] if amounts_found else None
+        payer = payer_name_found[0] if payer_name_found else None
+        cuit = cuit_found[0] if cuit_found else None
+        proof_number = proof_number_found[0] if proof_number_found else None
+
+    elif bank_name == "BBVA":
+
+        #for payer name
+        line_4 = lines[4]
+        line_4 = re.sub(r'^\w+:+\s+\b', '', line_4)
+        line_4 = re.sub(r'\b\s+\w+$', '', line_4)
+
+        #for proof number
+        line_3 = lines[3]
+        Cuenta_Origen = r'^Cuenta Origen:\s+CC\s+\$\s+\d{4}-\d{6}\/\d{1}\s+'
+        line_3 = re.sub(Cuenta_Origen, '', line_3)
+        line_3 = re.sub(r' ' , '', line_3)
+
+        #for amount
+        line_12 = lines[12]
+        #removes 1st word
+        line_12 = re.sub(r'^\w+:+\s+\b', '', line_12)
+        #adds $ 
+        line_12 = '$ ' + line_12
+        line_12 = re.sub(r'\b,00+$', '', line_12)
+
+
+        bank_pattern = 'Banco Santa Fe'
+        date_pattern = r'\b\d{1,2}/\d{1,2}/\d{4}\b'
+        #something is wrong here
+        amount_pattern = line_12
+        payer_name_pattern = line_4
+        proof_number_pattern = line_3
+
+        cuit_pattern = r'\b\d{2}-\d{8}-\d{1}\b'
+
+        bank_found = re.findall(bank_pattern, extracted_text, re.IGNORECASE)
+        dates_found = re.findall(date_pattern, extracted_text)
+        amounts_found = re.findall(amount_pattern, extracted_text)
+        payer_name_found = re.findall(payer_name_pattern, extracted_text)
+        cuit_found = re.findall(cuit_pattern, extracted_text)
+        proof_number_found = re.findall(proof_number_pattern, extracted_text)
+
+        bank = bank_found[0] if bank_found else None
+        date = dates_found[0] if dates_found else None
+        amount = line_12
+        payer = payer_name_found[0] if payer_name_found else None
+        cuit = cuit_found[0] if cuit_found else None
+        proof_number = line_3
+
+    elif bank_name == "Naranja X":
+
+        #for date
+        line_8 = lines[8]
+        line_8 = re.sub(r'^\w+\s+\w+\s+\b|\b\s-\s\d{2}:\d{2}\s+h$', '', line_8)
+        #for extracting payer name
+        line_14 = lines[14] 
+
+
+        bank_pattern = 'Naranja X'
+        date_pattern = line_8
+        amount_pattern = r'\$\s*\d+\.?\d*'
+        payer_name_pattern = line_14
+        proof_number_pattern = r'\b\d{10}\b'
+
+        cuit_pattern = r'\b\d{11}\b'
+
+        bank_found = re.findall(bank_pattern, extracted_text, re.IGNORECASE)
+        dates_found = re.findall(date_pattern, extracted_text)
+        amounts_found = re.findall(amount_pattern, extracted_text)
+        payer_name_found = re.findall(payer_name_pattern, extracted_text)
+        cuit_found = re.findall(cuit_pattern, extracted_text)
+        proof_number_found = re.findall(proof_number_pattern, extracted_text)
+
+        bank = bank_found[0] if bank_found else None
+        date = dates_found[0] if dates_found else None
+        amount = amounts_found[0] if amounts_found else None
+        payer = payer_name_found[0] if payer_name_found else None
+        cuit = cuit_found[0] if cuit_found else None
+        proof_number = proof_number_found[0] if proof_number_found else None
+
+    elif bank_name == "Banco Credicoop Coop. Ltdo":
+
+        # #for bank name if in case it is not detected uncomment below
+        # line_2 = lines[2] #CREDICOOP Banco Credicoop Coop. Ltdo
+        # line_2 = re.sub(r'^\w+\s+\b', '', line_2)
+
+
+        bank_pattern = 'Banco Credicoop Coop. Ltdo' #line_2
+        date_pattern = r'\b\d{1,2}/\d{1,2}/\d{4}\b'
+        amount_pattern = r'\$\s*\d+\.?\d*' #r'\$\s*\d[\d,\.]*'
+        payer_name_pattern = 'None'
+        proof_number_pattern = r'\b\d{9}\b'
+
+        cuit_pattern = r'\b\d{2}-\d{8}-\d{1}\b'
+
+        bank_found = re.findall(bank_pattern, extracted_text, re.IGNORECASE)
+        dates_found = re.findall(date_pattern, extracted_text)
+        amounts_found = re.findall(amount_pattern, extracted_text)
+        payer_name_found = re.findall(payer_name_pattern, extracted_text)
+        cuit_found = re.findall(cuit_pattern, extracted_text)
+        proof_number_found = re.findall(proof_number_pattern, extracted_text)
+
+        bank = bank_found[0] if bank_found else None
+        date = dates_found[0] if dates_found else None
+        amount = amounts_found[1] if amounts_found else None
+        amount = amount.replace('$', '$ ')
+        payer = payer_name_found[0] if payer_name_found else None
+        cuit = cuit_found[0] if cuit_found else None
+        proof_number = proof_number_found[0] if proof_number_found else None
+
+    elif bank_name == "Personal Pay":
+
+        #if the amount has 1 in it, the OCR is dectecting it as ] that's why we have to check the first line
+        if lines[0] == 'personal pay':
+
+            #for amount
+            line_4 = lines[4]
+            line_4 = re.sub(r'"', '', line_4) # remove '' in the end of the amount
+            line_4 = re.sub(r'(?<=\$)(?=\d)', r' ', line_4) # adds '$ ' in place of '$'
+
+            line_39 = lines[39] #43 for first proof number
+            line_40 = line_39 + lines[40] #44 for second proof number
+
+            #for payer name
+            line_28 = lines[28]
+
+        bank_pattern = 'Personal Pay'
+        date_pattern = r'\b\d{1,2}/\d{1,2}/\d{4}\b'
+        amount_pattern = line_4
+        payer_name_pattern = line_28
+        proof_number_pattern = line_40
+
+        cuit_pattern = r'\b\d{2}-\d{8}-\d{1}\b'
+
+        bank_found = re.findall(bank_pattern, extracted_text, re.IGNORECASE)
+        dates_found = re.findall(date_pattern, extracted_text)
+        amounts_found = re.findall(amount_pattern, extracted_text)
+        payer_name_found = re.findall(payer_name_pattern, extracted_text)
+        cuit_found = re.findall(cuit_pattern, extracted_text)
+        proof_number_found = re.findall(proof_number_pattern, extracted_text)
+
+        bank = bank_found[0] if bank_found else None
+        date = dates_found[0] if dates_found else None
+        amount = line_4
+        payer = line_28
+        cuit = cuit_found[0] if cuit_found else None
+        proof_number = line_40
+
+    elif bank_name == "Bancor":
+
+        #for proof number
+        line_3 = lines[3] 
+        #for payer name
+        line_11 = lines[11]
+        line_11 = re.sub(r'^\w+:+\s+\b', '', line_11)
+
+        bank_pattern = 'Bancor'
+        date_pattern = r'\b\d{1,2}/\d{1,2}/\d{4}\b'
+        amount_pattern = r'\$\s*\d+\.?\d*' #r'\$\s*\d[\d,\.]*'
+        proof_number_pattern = line_3
+        payer_name_pattern = line_11
+        cuit_pattern = r'\b\d{11}\b'  # MARTIN SAID Cuit IS FILLED MANUALLY
+
+        bank_found = re.findall(bank_pattern, extracted_text, re.IGNORECASE)
+        dates_found = re.findall(date_pattern, extracted_text)
+        amounts_found = re.findall(amount_pattern, extracted_text)
+        payer_name_found = re.findall(payer_name_pattern, extracted_text)
+        cuit_found = re.findall(cuit_pattern, extracted_text)
+        proof_number_found = re.findall(proof_number_pattern, extracted_text)
+
+        bank = bank_found[0] if bank_found else None
+        date = dates_found[0] if dates_found else None
+        amount = amounts_found[1] if amounts_found else None
+        payer = payer_name_found[0] if payer_name_found else None
+        cuit = cuit_found[0] if cuit_found else None
+        proof_number = proof_number_found[0] if proof_number_found else None
+
+
+
     # Return the extracted data
     return {
         'BANCO': bank_name,
@@ -310,4 +539,21 @@ def check_image_and_padding(folder_path):
 
 # Process images in the folder and save Excel file
 check_image_and_padding(folder_path)
-wb.save('Extracted_Data.xlsx')
+# wb.save('Extracted_Data.xlsx')
+
+from datetime import date
+
+today = date.today()
+formatted_date = today.strftime("%d_%m_%Y")
+
+# Specify the path to the extracted file
+extracted_file_path = f'{formatted_date}_extracted_info.xlsx'
+
+
+# Save the Excel file
+wb.save(extracted_file_path)
+import subprocess
+import os
+
+# Open the file with LibreOffice in linux
+subprocess.run(['libreoffice', extracted_file_path])
