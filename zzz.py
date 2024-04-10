02265fe7a9d811f39fd29805bb8d65673512e536
pr3Cash <blogger.thomasjerry@gmail.com>
2024-04-10 19:01:36 +0530

    idk what i am doing

--- zzz.py
+++ zzz.py
@@ -4,75 +4,47 @@ from openpyxl.styles import Font, Alignment, PatternFill
 from decimal import Decimal
 import cv2
 import pytesseract
+from pdf2image import convert_from_path
 import re
 
-#global variable for serial number
+# global variable for serial number
 index = 1
-last_amt = [] #list
+last_amt = []  # list
 
-folder_path = 'source' 
-image_path = 'source'
+folder_path = 'zource'
+image_path = 'zource'
 
-pdf_files = [file for file in os.listdir(folder_path) if file.endswith('.pdf')]
-
-all_files = [file for file in os.listdir(folder_path)] # if file.endswith('.pdf')]
+all_files = [file for file in os.listdir(folder_path)]
 print(all_files)
-"""
-global bank_found, dates_found, amounts_found, payer_name_found, cuit_found, proof_number_found, extracted_text, bank, date, amount, payer, cuit, proof_number, bank_pattern, date_pattern, amount_pattern, proof_number_pattern, payer_name_pattern, cuit_pattern 
-# Get the list of PDF files in the folder
-def pdf_to_image(pdf_folder_path):    
 
+# Get the list of PDF files in the folder and convert them to images
+def pdf_to_image(pdf_folder_path):
     pdf_files = [file for file in os.listdir(pdf_folder_path) if file.endswith('.pdf')]
-    
+
     # Convert each PDF file to images
     for pdf_file in pdf_files:
         # Construct the file paths
         pdf_file_path = os.path.join(pdf_folder_path, pdf_file)
         image_file_path = os.path.join(pdf_folder_path, os.path.splitext(pdf_file)[0] + '.png')
-    
+
         # Convert PDF to list of PIL images
         images = convert_from_path(pdf_file_path)
-    
+
         # Save each page of the PDF as an image file
         for i, image in enumerate(images):
             image.save(image_file_path, 'PNG')
-    
+
     print('PDFs converted to images successfully.')
 
-#extract the image to text
-def extract_image_to_text(image_path):    
-    # global extracted_text
+# Extract the image to text
+def extract_image_to_text(image_path):
+    global extracted_text
 
     image = cv2.imread(image_path)
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     extracted_text = pytesseract.image_to_string(gray_image)
 
-def details_regEx_patterns():
-    # global bank_found, dates_found, amounts_found, payer_name_found, cuit_found, proof_number_found
-
-    bank_found = re.findall(bank_pattern, extracted_text, re.IGNORECASE)
-    
-    dates_found = re.findall(date_pattern, extracted_text)
-    amounts_found = re.findall(amount_pattern, extracted_text)
-    payer_name_found = re.findall(payer_name_pattern , extracted_text)
-    cuit_found = re.findall(cuit_pattern, extracted_text)
-    proof_number_found = re.findall(proof_number_pattern, extracted_text)
-
-    return bank_found, dates_found, amounts_found, payer_name_found, cuit_found, proof_number_found
-
-# Extract the first date, amount, and CUIT number if any are found
-def extract_details():
-    # global bank, date, amount, payer, cuit, proof_number
-    
-    bank = bank_found[0] if bank_found else None
-    date = dates_found[0] if dates_found else None
-    amount = amounts_found[0] if amounts_found else None
-    proof_number = proof_number_found[0] if proof_number_found else None
-    
-    payer = payer_name_found[0] if payer_name_found else None
-    cuit = cuit_found[0] if cuit_found else None
-
-#return the extracted details
+# Return the extracted details
 def get_extracted_details(bank, date, amount, payer, cuit, proof_number):
     return {
         'BANCO': bank,
@@ -83,13 +55,13 @@ def get_extracted_details(bank, date, amount, payer, cuit, proof_number):
         'NRO COMPROBANTE': proof_number
     }
 
-#new Excel workbook
+# New Excel workbook
 wb = Workbook()
 ws = wb.active
 ws.title = "Extracted Data"
 
 # Add headers to the Excel sheet
-headers = [ 'Serie', 'FECHA', 'IMPORTE', 'NRO COMPROBANTE', 'TITULAR', 'CUIT', 'BANCO']
+headers = ['Serie', 'FECHA', 'IMPORTE', 'NRO COMPROBANTE', 'TITULAR', 'CUIT', 'BANCO']
 ws.append(headers)
 
 # Apply bold style to the header cells
@@ -102,8 +74,7 @@ for row in ws.iter_rows():
     for cell in row:
         cell.alignment = Alignment(horizontal='center', vertical='center', wrap_text=True)
 
-#check if image exists and adjust the padding
-
+# Check if image exists and adjust the padding
 def check_image_and_padding(folder_path):
     global index, last_amt
 
@@ -115,20 +86,18 @@ def check_image_and_padding(folder_path):
             image_path = os.path.join(folder_path, filename)
             extracted_data = extract_data_from_image(image_path)
 
-            row_data = [index] # Add the serial number as the first element
+            row_data = [index]  # Add the serial number as the first element
             row_data.extend([extracted_data.get(header) for header in headers[1:]])
             ws.append(row_data)
-            index += 1            
+            index += 1
 
             # Update total sum formula dynamically
             if extracted_data['IMPORTE']:
                 amt = re.sub(r'[^\$\s0-9.]', '', extracted_data['IMPORTE'])
                 num_amt = Decimal(re.sub(r'[^\d.]', '', amt))
-                # total_sum_formula += num_amt
-
                 last_amt.append(num_amt)
                 print(f"Current total sum: {sum(last_amt)}")
-            
+
             # Apply yellow fill to cells that are empty in the row
             if any(is_empty(cell.value) for cell in ws[ws.max_row]):
                 for cell in ws[ws.max_row]:
@@ -151,61 +120,64 @@ def check_image_and_padding(folder_path):
 def is_empty(cell_value):
     return cell_value is None or cell_value == ''
 
-os.listdir(folder_path)
+# Process each image in the folder
+def extract_data_from_image(image_path):
+    extract_image_to_text(image_path)
+
+    lines = extracted_text.split('\n')
+    line = lines[0].lower()
+
+    if line == 'bancopatagonia':
+        bank_pattern = 'Bancopatagonia'
+        date_pattern = r'\b\d{1,2}/\d{1,2}/\d{4}\b'
+        amount_pattern = r'\$\s*\d[\d,\.]*'
+        proof_number_pattern = r'\b\d{10}\b'
+        payer_name_pattern = lines[11]
+        cuit_pattern = 'None'  # MARTIN SAID Cuit IS FILLED MANUALLY
+
+        bank_found = re.findall(bank_pattern, extracted_text, re.IGNORECASE)
+        dates_found = re.findall(date_pattern, extracted_text)
+        amounts_found = re.findall(amount_pattern, extracted_text)
+        payer_name_found = re.findall(payer_name_pattern, extracted_text)
+        cuit_found = re.findall(cuit_pattern, extracted_text)
+        proof_number_found = re.findall(proof_number_pattern, extracted_text)
+
+        bank = bank_found[0] if bank_found else None
+        date = dates_found[0] if dates_found else None
+        amount = amounts_found[1] if amounts_found else None
+        payer = payer_name_found[0] if payer_name_found else None
+        cuit = cuit_found[0] if cuit_found else None
+        proof_number = proof_number_found[0] if proof_number_found else None
+
+        return get_extracted_details(bank, date, amount, payer, cuit, proof_number)
+
+    if line == 'galicia':
+        date_pattern = r'\b\d{1,2}/\d{1,2}/\d{4}\b'
+        amount_pattern = r'\$\s*\d[\d,\.]*'
+        proof_number_pattern = r'\b\d{11}\b'
+        payer_name_pattern = lines[8]
+        cuit_pattern = r'\b\d{2}-\d{8}-\d{1}\b'
+
+        bank_found = re.findall(bank_pattern, extracted_text, re.IGNORECASE)
+        dates_found = re.findall(date_pattern, extracted_text)
+        amounts_found = re.findall(amount_pattern, extracted_text)
+        payer_name_found = re.findall(payer_name_pattern, extracted_text)
+        cuit_found = re.findall(cuit_pattern, extracted_text)
+        proof_number_found = re.findall(proof_number_pattern, extracted_text)
+
+        bank = bank_found[0] if bank_found else None
+        date = dates_found[0] if dates_found else None
+        amount = amounts_found[0] if amounts_found else None
+        payer = payer_name_found[0] if payer_name_found else None
+        cuit = cuit_found[0] if cuit_found else None
+
+        if any(len(p) == 9 for p in proof_number_found) and any(len(p) == 11 for p in proof_number_found):
+            proof_number = next((p for p in proof_number_found if len(p) == 9), None)
+        else:
+            proof_number = proof_number_found[0] if proof_number_found else None
+
+        return get_extracted_details(bank, date, amount, payer, cuit, proof_number)
 
 # Process each image in the folder
-def extract_data_from_image(folder_path):
-            pdf_to_image(folder_path)
-        
-            extract_image_to_text(folder_path)
-        
-            lines = extracted_text.split('\n')
-            line = lines[0].lower()
-    
-            if line == 'bancopatagonia':
-                    # global bank_pattern, date_pattern, amount_pattern, proof_number_pattern, payer_name_pattern, cuit_pattern
-                    bank_pattern = 'Bancopatagonia'
-                    date_pattern = r'\b\d{1,2}/\d{1,2}/\d{4}\b'
-                    amount_pattern = r'\$\s*\d[\d,\.]*'
-                    proof_number_pattern = r'\b\d{10}\b'
-                    payer_name_pattern = lines[11]
-                    cuit_pattern =  'None' #MARTIN SAID Cuit IS FILLED MANUALLY 
-    
-                    # Find all occurrences of dates, amounts, and CUIT numbers in the extracted text
-                    details_regEx_patterns()
-                    # Extract the first date, amount, and CUIT number if any are found
-                    extract_details()
-                    amount = amounts_found[1] if amounts_found else None
-                    # Return the extracted information as a dictionary
-                    return get_extracted_details(bank, date, amount, payer, cuit, proof_number)
-                # extract_data_from_image()
-    
-            if line == 'galicia':
-                # def extract_data_from_image():
-                    # global bank_pattern, date_pattern, amount_pattern, proof_number_pattern, payer_name_pattern, cuit_pattern
-                    date_pattern = r'\b\d{1,2}/\d{1,2}/\d{4}\b'
-                    amount_pattern = r'\$\s*\d[\d,\.]*'
-                    proof_number_pattern = r'\b\d{11}\b'
-                    payer_name_pattern = lines[8]
-                    cuit_pattern =  r'\b\d{2}-\d{8}-\d{1}\b'
-    
-                    # Find all occurrences of dates, amounts, and CUIT numbers in the extracted text
-                    details_regEx_patterns()
-                    # Extract the data if found
-                    extract_details()
-                    # Check if both 9-digit and 11-digit proof numbers are present
-                    if any(len(p) == 9 for p in proof_number_found) and any(len(p) == 11 for p in proof_number_found):
-                        # Extract the 9-digit proof number if both are present
-                        proof_number = next((p for p in proof_number_found if len(p) == 9), None)
-                    else:
-                        # Extract the first found proof number (either 9-digit or 11-digit)
-                        proof_number = proof_number_found[0] if proof_number_found else None
-                    # Return the extracted information as a dictionary
-                    return get_extracted_details(bank, date, amount, payer, cuit, proof_number)
-                # extract_data_from_image()
-            
-# folder_path = 'source'
 check_image_and_padding(folder_path)
-process_images(folder_path)
 wb.save('Extracted_Data.xlsx')
-"""
